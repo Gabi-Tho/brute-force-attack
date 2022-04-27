9e3a75c0b87e0be49d51dac1aae85c98c0e17d47
Gabi-Tho <e2194469@cmaisonneuve.qc.ca>
2022-04-27 15:14:20 -0400

    make into function

--- brute-force.php
+++ brute-force.php
@@ -1,26 +1,37 @@
 <?php
+
+function BruteForcePass($userPassword){
+
 $chars=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",0,1,2,3,4,5,6,7,8,9];
 $mdp3="";
 $mdp4="";
 $mdp5="";
 
-for ($i=0; $i< count($chars); $i++){
-    for ($j=0; $j< count($chars); $j++){
-        for ($k=0; $k< count($chars); $k++){
-            $mdp3= $chars[$i].$chars[$j].$chars[$k];
-            if ($mdp3 == "hb1"){
-                echo "correct password";
-            }
-            for ($l=0; $l< count($chars); $l++){
-                $mdp4= $chars[$i].$chars[$j].$chars[$k].$chars[$l];
-                for ($m=0; $m< count($chars); $m++){
-                    $mdp5= $chars[$i].$chars[$j].$chars[$k].$chars[$l].$chars[$m];
+    for ($i=0; $i< count($chars); $i++){
+        for ($j=0; $j< count($chars); $j++){
+            for ($k=0; $k< count($chars); $k++){
+                $mdp3= $chars[$i].$chars[$j].$chars[$k];
+                if ($mdp3 == $userPassword){
                 }
+                for($l=0; $l< count($chars); $l++){
+                    $mdp4= $chars[$i].$chars[$j].$chars[$k].$chars[$l];
+                    if($mdp4 == $userPassword){
+                        }
+                        for ($m = 0; $m< count($chars); $m++){
+                            $mdp5 = $chars[$i].$chars[$j].$chars[$k].$chars[$l].$chars[$m];
+                            if($mdp5 == $userPassword){
+                            }
+    
+                        }    
+    
+                }
+    
             }
-            
         }
     }
-
+    
 }
 
+
+echo BruteForcePass("hb1");
 ?>
